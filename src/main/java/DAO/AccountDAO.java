7a9d9cb5b59ce93afdabfb99db66ad42c0811337
Eddie Padilla <dediosprogramming@gmail.com>
2023-05-02 17:48:04 +0000

    checkDuplicateUserName implemented

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -14,34 +14,48 @@ public class AccountDAO {
     // create method to check for duplicate usernames in the database
 
 
-    public Account checkDuplicateUserName(String str) throws SQLException {
+    public String checkDuplicateUserName(String str) {
 
-        // create connection to database
-        Connection connection = ConnectionUtil.getConnection();
+        try{
+            // create connection to database
+            Connection connection = ConnectionUtil.getConnection();
 
-        // sql statement
-        String sql = "SELECT * FROM account WHERE username=?";
+            // sql statement
+            String sql = "SELECT * FROM account WHERE username=?";
 
-        // precompiled sql statement
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            // precompiled sql statement
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-        // sets username to value of str argument
-        preparedStatement.setString(2, str);
+            // sets username to value of str argument
+            preparedStatement.setString(2, str);
 
-        // executes the query
-        ResultSet rs = preparedStatement.executeQuery();
+            // executes the query
+            ResultSet rs = preparedStatement.executeQuery();
 
-        // while (rs.next())
-        // create a new instance of account to return - when returning an account from db you need all three constructors
-        // Account existingAccount = a username already in the database;
-        // return existingAccount;
+            // while (rs.next())
+            // create a new instance of account to return - when returning an account from db you need all three constructors
+            // Account existingAccount = rs.getusername;
+            // return existingAccount;
 
-        while (rs.next()) {
-            Account existingAccount = new Account (Account.getAccount_id(), rs.getUsername(), Account.getPassword());
+            Account rsCast = (Account) rs;
 
-            return existingAccount;
+            while (rs.next()) {
+            
+                Account existingAccount = new Account(rsCast.getUsername(), rsCast.getPassword());
+            
+                return existingAccount.getUsername().toString();
+            
+            }
+
+
+            
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+
         }
+        return null; 
 
+        
 
 
 
