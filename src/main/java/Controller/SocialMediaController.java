707eb9f3379b18baa6970ede7aa0586570ca073c
Eddie Padilla <dediosprogramming@gmail.com>
2023-05-02 19:16:42 +0000

    planning implementations

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -25,11 +25,28 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.post("/register", this::newUserRegistration);
+        app.post("/login", this::userLogin);
+        app.post("/messages", this::createNewMessage);
 
         return app;
     }
 
+
+
+
+
+    /* ## 1: Our API should be able to process new User registrations.
+
+As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. The body will contain a representation of a JSON Account, but will 
+not contain an account_id.
+
+- The registration will be successful if and only if the username is not blank, the password is at least 4 
+characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a 
+JSON of the Account, including its account_id. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
+- If the registration is not successful, the response status should be 400. (Client error) 
+*/
     /**
+     * 
      *
      * @param ctx The Javalin Context object manages information about both the HTTP request and response.
      * @throws JsonProcessingException
@@ -64,18 +81,73 @@ public class SocialMediaController {
     }
 
 
+    /* ## 2: Our API should be able to process User logins.
+
+As a user, I should be able to verify my login on the endpoint POST localhost:8080/login. 
+The request body will contain a JSON representation of an Account, not containing an account_id. 
+In the future, this action may generate a Session token to allow the user to securely use the site. We will not worry about this for now.
+
+- The login will be successful if and only if the username and password provided in the request body JSON match a real account existing on the database. 
+If successful, the response body should contain a JSON of the account in the response body, including its account_id. 
+The response status should be 200 OK, which is the default.
+- If the login is not successful, the response status should be 401. (Unauthorized) */
+
+    private void userLogin(Context ctx) throws JsonMappingException, JsonProcessingException {
+
+        // object mapper instance
+        ObjectMapper mapper = new ObjectMapper();
+
+        // allows me to read the created fields of a new user account
+        Account account = mapper.readValue(ctx.body(), Account.class);
+
+        // if username and password match a real account on the db - successful login
+        // if (account.username && account.password == "an account you queried in DAO - getAccountByUsernameAndPassword")
+        // ctx.status(200);
+        // else not successful
+        // ctx.status(401) - unauthorized
+
+
+    } 
+
+
+    /* ## 3: Our API should be able to process the creation of new messages.
+
+As a user, I should be able to submit a new post on the endpoint POST localhost:8080/messages. The request body will contain a JSON representation of a message, 
+which should be persisted to the database, but will not contain a message_id.
+
+- The creation of the message will be successful if and only if the message_text is not blank, 
+is under 255 characters, and posted_by refers to a real, existing user. If successful, the response body should contain a JSON of the message, 
+including its message_id. The response status should be 200, which is the default. The new message should be persisted to the database.
+- If the creation of the message is not successful, the response status should be 400. (Client error)
+ */
+
+    private void createNewMessage(Context ctx) throws JsonMappingException, JsonProcessingException {
+
+        // object mapper instance to read message
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        // successful if
+        // message_text != "" || message_text.length() < 255 || posted_by == account_id(posted_by is fkey to account_id - DAO - get account by posted_by )
+        // ctx.status(200)
+        // ctx.json(message) - including message_id
+        // else 
+        // ctx.status(400) - client error
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
 
 
 
 
 
-/* ## 1: Our API should be able to process new User registrations.
-
-As a user, I should be able to create a new Account on the endpoint POST localhost:8080/register. The body will contain a representation of a JSON Account, but will 
-not contain an account_id.
-
-- The registration will be successful if and only if the username is not blank, the password is at least 4 
-characters long, and an Account with that username does not already exist. If all these conditions are met, the response body should contain a 
-JSON of the Account, including its account_id. The response status should be 200 OK, which is the default. The new account should be persisted to the database.
-- If the registration is not successful, the response status should be 400. (Client error) */
