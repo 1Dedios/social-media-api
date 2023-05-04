f6a7ffb6ad8d359de92901df00ee10296f1871a1
Eddie Padilla <dediosprogramming@gmail.com>
2023-05-04 19:03:23 +0000

    implementing insertNewMessage

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -15,24 +15,6 @@ public class MessageDAO {
 
     public static Message insertNewMessage (Message message) {
 
-        // Connection connect = ConnectionUtil.getConnection();
-
-        /* place these within a try catch block */
-
-        // String sql = "INSERT INTO message (posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?);";
-
-        // PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-        // preparedStatement.setInt(1, message.posted_by);
-        // preparedStatement.setString(2, message.message_text);
-        // preparedStatement.setBigInt(3, message.time_posted_epoch);
-
-        // ResultSet rs = preparedStatement.executeUpdate();
-
-        // while (rs.next())
-        // Message newMessage = new Message (rs.getInt("posted_by"), rs.getString("message_text"), rs.getBigInt("time_posted_epoch"));
-        // return new Message;
-
         Connection connect = ConnectionUtil.getConnection();
 
         try {
@@ -50,7 +32,8 @@ public class MessageDAO {
 
             if (pKeyResultSet.next()) {
                 int generated_message_id = pKeyResultSet.getInt(1);
-                return new Message (generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
             }
 
         } catch (SQLException e) {
